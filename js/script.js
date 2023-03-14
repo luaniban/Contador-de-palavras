3b00adca99f832dcb2fa6188676a92022d7c8f5d
luaniban <luanibanhez@gmail.com>
2023-03-14 11:12:58 -0300

    Projeto contador de palavras finalizado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function calcular(){
+    let text = document.getElementById('texto').value;
+
+    let result = text.split(' ').length;
+
+    document.getElementById('resultado').innerHTML = 'O texto possui: ' + result + 'palavras';
+    
+}
